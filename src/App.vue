08bd81204c5ab5a1ed6dd7e711f4c0c46db37789
Lisa Huang <lisahuangade19@gmail.com>
2022-12-06 16:02:13 +1030

    not done yet - category

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
 
   <div class="container">
-    <Search />
-    <List />
+    <Category />
+    <Category />
+    <Category />
   </div>
   
 </template> 
 
 <script>
 
-import Search from './components/Search'
-import List from './components/List'
+import Category from './components/Category'
 
 export default {
   name: "App",
   components: {
-    Search,
-    List
+    Category
   },
- 
-
+  data() {
+    return {
+      foods:['hotpot', 'pizza', 'noodle', 'steak'],
+      games:['red', 'green', 'dance', 'poker'],
+      films:['God Father', 'Expert', 'Square or There', 'Vue']
+    }
+  }
 };
 </script>
 
+<style>
+  .container {
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
---
+++ src/components/Category.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="category">
+    <h3>xxxxx</h3>
+    <ul>
+        <li>xxxxxx</li>
+        <li>xxxxxx</li>
+        <li>xxxxxx</li>
+        <li>xxxxxx</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'category'
+}
+</script>
+
+<style>
+    .category {
+        background-color: skyblue;
+        width: 200px;
+        height: 300px;
+    }
+    h3 {
+        text-align: center;
+        background-color: orange;
+    }
+</style>
